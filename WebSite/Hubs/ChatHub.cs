6625dd8d0642249f28654a89f6887faf2660ff00
Kovalenko Alena <kovalenkoalena789@gmail.com>
2021-02-22 21:16:58 +0300

    ready send message to user

--- WebSite/Hubs/ChatHub.cs
+++ WebSite/Hubs/ChatHub.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
+using System.Collections.Generic;
+using System.Linq;
+
 using WebSite.Models;
 
 namespace WebSite.Hubs
@@ -11,13 +14,115 @@ namespace WebSite.Hubs
     [Authorize]
     public class ChatHub : Hub
     {
+
+        private readonly static ConnectionMapping<string> _connections =
+            new ConnectionMapping<string>();
+
+        public override Task OnConnected()
+        {
+            string name = Context.User.Identity.Name;
+            _connections.Add(name, Context.ConnectionId);
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            string name = Context.User.Identity.Name;
+            _connections.Remove(name, Context.ConnectionId);
+            return base.OnDisconnected(stopCalled);
+        }
+
+        public override Task OnReconnected()
+        {
+            string name = Context.User.Identity.Name;
+            if (!_connections.GetConnections(name).Contains(Context.ConnectionId))
+            {
+                _connections.Add(name, Context.ConnectionId);
+            }
+            return base.OnReconnected();
+        }
+
+
         public async Task Send(string from, string toUserId, string message)
         {
             ///string to = toUserId.Get;
-            await Clients.Client(toUserId).sendMessageAsync(from, message);
+            foreach (var connectionId in _connections.GetConnections(toUserId))
+            {
+                await Clients.Client(Context.ConnectionId).sendMessageAsync(from, message);
+                await Clients.Client(connectionId).sendMessageAsync(from, message);
+            }
+            
             //await Clients.User(Context.ConnectionId).sendMessageAsync(from, message);
         }
 
         public string GetConnectionId() => Context.ConnectionId;
     }
+
+
+    public class ConnectionMapping<T>
+    {
+        private readonly Dictionary<T, HashSet<string>> _connections =
+            new Dictionary<T, HashSet<string>>();
+
+        public int Count
+        {
+            get
+            {
+                return _connections.Count;
+            }
+        }
+
+        public void Add(T key, string connectionId)
+        {
+            lock (_connections)
+            {
+                HashSet<string> connections;
+                if (!_connections.TryGetValue(key, out connections))
+                {
+                    connections = new HashSet<string>();
+                    _connections.Add(key, connections);
+                }
+
+                lock (connections)
+                {
+                    connections.Add(connectionId);
+                }
+            }
+        }
+
+        public IEnumerable<string> GetConnections(T key)
+        {
+            HashSet<string> connections;
+            if (_connections.TryGetValue(key, out connections))
+            {
+                return connections;
+            }
+
+            return Enumerable.Empty<string>();
+        }
+
+        public void Remove(T key, string connectionId)
+        {
+            lock (_connections)
+            {
+                HashSet<string> connections;
+                if (!_connections.TryGetValue(key, out connections))
+                {
+                    return;
+                }
+
+                lock (connections)
+                {
+                    connections.Remove(connectionId);
+
+                    if (connections.Count == 0)
+                    {
+                        _connections.Remove(key);
+                    }
+                }
+            }
+        }
+    }
+
+
 }
